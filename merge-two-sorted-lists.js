2a70d60b71f92a1bd74debfb99baf8a612c9be10
Seth Carter <carter.seth.m@gmail.com>
2020-10-03 13:43:41 +0100

    Added an algorithm solution for removing duplicates from a sorted array

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -1,3 +1,4 @@
+//Link to leetcode problem: https://leetcode.com/problems/merge-two-sorted-lists/
 
 //  Definition for singly-linked list.
 //  function ListNode(val, next) {
---
+++ remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+//Link to leetcode problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+const removeDuplicates = function(nums) {
+    let i = 0
+    for(let j = i; j < nums.length; j++){
+      if (nums[j] !== nums[i]){
+      i++;
+      nums[i] = nums[j]
+      }
+  }
+  return i+1
+}
+
+removeDuplicates(nums)
