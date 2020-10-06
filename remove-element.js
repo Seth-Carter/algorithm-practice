ce34c0aa4123cdb526e940b22c56ce04723d33ca
Seth Carter <carter.seth.m@gmail.com>
2020-10-06 16:42:42 +0100

    Added a solution for removing elements from an array in place

---
+++ remove-element.js
@@ -0,0 +1,17 @@
+//Link to leetcode problem: https://leetcode.com/problems/remove-element/
+
+const removeElement = function(nums, val) {
+    let i = 0
+    for (let j = 0; j < nums.length; j++){
+      if (nums[j] !== val ){
+        nums[i] = nums[j]
+        i++
+      }
+    }
+    return i
+}
+
+
+const testArray = [2,3,3,2]
+
+removeElement(testArray, 3)
