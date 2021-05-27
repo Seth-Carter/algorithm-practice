65b7b355819468f390e0584db022d3b1e44bbe83
Seth-Carter <carter.seth.m@gmail.com>
2021-05-27 21:00:04 +0100

    Added upper case handling to anagram checker.

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,18 +1,21 @@
 // Solution to the common anagram algorithm problem
 
 function anagramChecker(word1, word2) {
-  if(array1.length !== array2.length ) return false;
+  if(word1.length !== word2.length ) return false;
   let counter1 = {};
   let counter2 = {};
   for(let letter of word1) {
-      counter1[letter] = ++counter1[letter] || 1;
+    let lowerLetter = letter.toLowerCase()
+    counter1[lowerLetter] = ++counter1[lowerLetter] || 1;
   }
   for (let letter of word2) {
-      counter2[letter] = ++counter2[letter] || 1;
+    let lowerLetter = letter.toLowerCase()
+    counter2[lowerLetter] = ++counter2[lowerLetter] || 1;
   }
+  console.log(counter1, counter2);
   for (let key in counter1) {
-      if (!counter2[key]) return false;
-      if (counter1[key] !== counter2[key]) return false;
+    if (!counter2[key]) return false;
+    if (counter1[key] !== counter2[key]) return false;
   }
   return true;
-}
+};
