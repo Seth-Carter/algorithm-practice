e870288a2941621f7b16dae0d13784ae7ad95b50
Seth-Carter <carter.seth.m@gmail.com>
2021-06-11 14:38:55 +0100

    Added a new algorithm

---
+++ collect-strings.js
@@ -0,0 +1,32 @@
+// An algorithm that accepts an object and returns an array
+// of all the values in the object that are strings.
+
+function collectStrings(obj) {
+  const result = [];
+  for (let value of Object.values(obj)) {
+    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+      result.push(...collectStrings(value));
+    } else if (typeof value === 'string') {
+      result.push(value);
+    }
+  }
+  return result;
+}
+
+const testObj = {
+  stuff: "foo",
+  data: {
+      val: {
+          thing: {
+              info: "bar",
+              moreInfo: {
+                  evenMoreInfo: {
+                      weMadeIt: "baz"
+                  }
+              }
+          }
+      }
+  }
+}
+
+console.log(collectStrings(testObj));
