69175bc6f9bd6f9258b50dba9ef1661e1488388f
Seth-Carter <carter.seth.m@gmail.com>
2021-06-22 19:42:39 +0100

    Added an optimized bubble sort algorithm.

---
+++ search-algorithms/bubble-sort.js
@@ -0,0 +1,18 @@
+function bubbleSort(array) {
+  let noSwap;
+  for (let i = array.length - 1; i > 0; i--) {
+    noSwap = true;
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return array;
+}
+
+const testArray = [2, 10, 1, 4, 5, 9, 6];
+
+console.log(bubbleSort(testArray));
