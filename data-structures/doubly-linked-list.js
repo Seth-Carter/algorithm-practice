913aac9d21ff5af7a8242ba7d5726e7f4fc60b88
Seth-Carter <carter.seth.m@gmail.com>
2021-07-13 09:32:45 +0100

    Added a doubly linked list class

---
+++ data-structures/doubly-linked-list.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.length) return undefined;
+    const removedTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      removedTail.prev = null;
+    }
+    this.length--;
+    return removedTail;
+  }
+  shift() {
+    if (!this.length) return undefined;
+    const removedHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      removedHead.next = null;
+    }
+    this.length--;
+    return removedHead;
+  }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+const testList = new DoublyLinkedList();
+testList.push(0);
+testList.push(1);
+testList.push(2);
+console.log(testList.unshift(-1));
