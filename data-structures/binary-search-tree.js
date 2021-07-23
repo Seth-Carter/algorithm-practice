4e7c81ab239d1be29b64cddf82948011f86f4dfc
Seth-Carter <carter.seth.m@gmail.com>
2021-07-23 08:32:40 +0100

    Added a recursive and iterative find method for BST

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -16,27 +16,47 @@ class BinarySearchTree {
     if (!this.root) {
       this.root = newNode;
     } else {
-      const search = (searchNode, nodeToInsert) => {
+      const recursiveHelper = (searchNode, nodeToInsert) => {
         if (searchNode.value === nodeToInsert.value) return ++searchNode.count;
         if (nodeToInsert.value > searchNode.value) {
           if (!searchNode.right) return (searchNode.right = nodeToInsert);
-          return search(searchNode.right, nodeToInsert);
+          return recursiveHelper(searchNode.right, nodeToInsert);
         } else {
           if (!searchNode.left) return (searchNode.left = nodeToInsert);
-          return search(searchNode.left, nodeToInsert);
+          return recursiveHelper(searchNode.left, nodeToInsert);
         }
       };
-      search(this.root, newNode);
+      recursiveHelper(this.root, newNode);
     }
     return this;
+  }
+  find(value, node = this.root) {
+    if (!node) return false;
+    if (node.value === value) return node;
+    if (value > node.value) return this.find(value, node.right);
+    return this.find(value, node.left);
+  }
+  iterativeFind(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while (current) {
+      if (current.value === value) return current;
+      if (value > current.value) {
+        current = current.right;
+      } else current = current.left;
+    }
+    return false;
   }
 }
 
 const testBst = new BinarySearchTree();
-console.log(testBst.insert(35));
-console.log(testBst.insert(35));
-console.log(testBst.insert(25));
-console.log(testBst.insert(45));
-console.log(testBst.insert(5));
-console.log(testBst.insert(12));
-console.log(testBst.insert(99));
+testBst.insert(35);
+testBst.insert(35);
+testBst.insert(25);
+testBst.insert(45);
+testBst.insert(5);
+testBst.insert(12);
+testBst.insert(99);
+testBst.insert(99);
+console.log(testBst.recursiveFind(12));
+console.log(testBst.iterativeFind(12));
