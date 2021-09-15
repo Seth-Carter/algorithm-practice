75b487f98eb04516d3cf89f993d30ba34dc05ae6
Seth-Carter <carter.seth.m@gmail.com>
2021-09-15 08:56:24 +0100

    Added a dequeue method to the priority queue class

--- data-structures/priority-queue.js
+++ data-structures/priority-queue.js
@@ -29,6 +29,45 @@ class PriorityQueue {
     };
     return bubbleUp();
   }
+  dequeue() {
+    const trickleDown = (index = 0, leftChildIndex = 1) => {
+      const rightChildIndex = leftChildIndex + 1;
+      let swap = null;
+      if (leftChildIndex < this.values.length) {
+        if (
+          this.values[index].priority > this.values[leftChildIndex].priority
+        ) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < this.values.length) {
+        if (
+          (!swap &&
+            this.values[index].priority >
+              this.values[rightChildIndex].priority) ||
+          (swap &&
+            this.values[rightChildIndex].priority <
+              this.values[leftChildIndex].priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap) {
+        [this.values[index], this.values[swap]] = [
+          this.values[swap],
+          this.values[index],
+        ];
+        trickleDown(swap, swap * 2 + 1);
+      }
+    };
+    const removed = this.values[0];
+    if (this.values.length > 0) {
+      this.values[0] = this.values[this.values.length - 1];
+      this.values.pop();
+      trickleDown();
+    }
+    return removed;
+  }
 }
 
 const queue = new PriorityQueue();
@@ -38,4 +77,8 @@ queue.enqueue('Clean bathroom', 5);
 queue.enqueue('Clean living room', 1);
 queue.enqueue('Clean garden', 60);
 queue.enqueue('Take shower', 0);
+console.log(queue.values);
+queue.dequeue();
+console.log(queue.values);
+queue.dequeue();
 console.log(queue.values);
