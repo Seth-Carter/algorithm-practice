dda8d5f3132709adae5dfe510d5f75937321b356
Seth-Carter <carter.seth.m@gmail.com>
2021-09-23 09:26:11 +0100

    Added methods for adding and removing edges to the graph class

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -5,4 +5,34 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList.hasOwnProperty(vertex1)) {
+      this.adjacencyList[vertex1] = [];
+    }
+    this.adjacencyList[vertex1].push(vertex2);
+    if (!this.adjacencyList.hasOwnProperty(vertex2)) {
+      this.adjacencyList[vertex2] = [];
+    }
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList.hasOwnProperty(vertex1)) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (vertex) => vertex !== vertex2
+      );
+    }
+    if (this.adjacencyList.hasOwnProperty(vertex2)) {
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (vertex) => vertex !== vertex1
+      );
+    }
+  }
 }
+
+const g = new Graph();
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Aspen', 'Dallas');
+g.addEdge('Tokyo', 'London');
+console.log(g);
+g.removeEdge('Dallas', 'Tokyo');
+console.log(g);
