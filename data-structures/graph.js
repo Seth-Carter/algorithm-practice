58d7c53101c270543ff187ba3609753d6c6d152c
Seth-Carter <carter.seth.m@gmail.com>
2021-09-24 11:17:23 +0100

    Added linting

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -6,22 +6,22 @@ class Graph {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
   addEdge(vertex1, vertex2) {
-    if (!this.adjacencyList.hasOwnProperty(vertex1)) {
+    if (!Object.prototype.hasOwnProperty.call(this.adjacencyList, vertex1)) {
       this.adjacencyList[vertex1] = [];
     }
     this.adjacencyList[vertex1].push(vertex2);
-    if (!this.adjacencyList.hasOwnProperty(vertex2)) {
+    if (!Object.prototype.hasOwnProperty.call(this.adjacencyList, vertex2)) {
       this.adjacencyList[vertex2] = [];
     }
     this.adjacencyList[vertex2].push(vertex1);
   }
   removeEdge(vertex1, vertex2) {
-    if (this.adjacencyList.hasOwnProperty(vertex1)) {
+    if (Object.prototype.hasOwnProperty.call(this.adjacencyList, vertex1)) {
       this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
         (vertex) => vertex !== vertex2
       );
     }
-    if (this.adjacencyList.hasOwnProperty(vertex2)) {
+    if (Object.prototype.hasOwnProperty.call(this.adjacencyList, vertex2)) {
       this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
         (vertex) => vertex !== vertex1
       );
