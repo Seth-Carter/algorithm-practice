843f95d752bd988451fcf3272fddad602546b754
Seth-Carter <carter.seth.m@gmail.com>
2021-10-13 08:53:57 +0100

    Added the beginnings of Dijkstra's algorithm

---
+++ algorithms/dijkstras.js
@@ -0,0 +1,39 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+  dijkstras(start, end) {
+    return;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+}
+
+const graph = new WeightedGraph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+
+graph.addEdge('A', 'B', 9);
+graph.addEdge('A', 'C', 5);
+graph.addEdge('B', 'C', 7);
+console.log(graph.adjacencyList);
