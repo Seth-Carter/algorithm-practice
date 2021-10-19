87d82b6d64a219c589fe04776bdddd85e4c3c8cc
Seth-Carter <carter.seth.m@gmail.com>
2021-10-19 08:48:51 +0100

    Combined binary heap priority queue with dijkstras

--- data-structures/priority-queue.js
+++ data-structures/priority-queue.js
@@ -1,3 +1,48 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+  dijkstras(start, end) {
+    let nodes = new PriorityQueue();
+    let distances = {};
+    let previous = {};
+    let path = [];
+    let currentVertex;
+    for (let vertex in this.adjacencyList) {
+      vertex === start
+        ? (distances[vertex] = 0)
+        : (distances[vertex] = Infinity);
+      previous[vertex] = null;
+    }
+    nodes.enqueue(start, distances[start]);
+    while (nodes.values.length) {
+      currentVertex = nodes.dequeue().val;
+      if (currentVertex === end) {
+        path.push(currentVertex);
+        while (previous[currentVertex]) {
+          path.push(previous[currentVertex]);
+          currentVertex = previous[currentVertex];
+        }
+        return path.reverse();
+      }
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        let potential = distances[currentVertex] + neighbor.weight;
+        if (potential < distances[neighbor.node]) {
+          distances[neighbor.node] = potential;
+          previous[neighbor.node] = currentVertex;
+          nodes.enqueue(neighbor.node, potential);
+        }
+      });
+    }
+  }
+}
 class Node {
   constructor(val, priority) {
     this.val = val;
@@ -69,16 +114,22 @@ class PriorityQueue {
     return removed;
   }
 }
+const graph = new WeightedGraph();
 
-const queue = new PriorityQueue();
-queue.enqueue('Clean kitchen', 20);
-queue.enqueue('Clean bedroom', 30);
-queue.enqueue('Clean bathroom', 5);
-queue.enqueue('Clean living room', 1);
-queue.enqueue('Clean garden', 60);
-queue.enqueue('Take shower', 0);
-console.log(queue.values);
-queue.dequeue();
-console.log(queue.values);
-queue.dequeue();
-console.log(queue.values);
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('E', 'F', 1);
+
+console.log(graph.dijkstras('A', 'E'));
