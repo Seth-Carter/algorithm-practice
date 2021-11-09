aa839a96186206c08d5e2d3485569d03d9f10c9a
Seth-Carter <carter.seth.m@gmail.com>
2021-11-09 08:52:56 +0000

    Added a log(n) time search insert position algorithm

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,6 +1,6 @@
 //Link to leetcode problem: https://leetcode.com/problems/search-insert-position/
 
-const searchInsert = function (nums, target) {
+const naiveSearchInsert = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === target) {
       return i;
@@ -10,3 +10,18 @@ const searchInsert = function (nums, target) {
   }
   return nums.length;
 };
+
+const searchInsert = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let pivot;
+  while (left <= right) {
+    pivot = Math.floor((left + right) / 2);
+    if (target === nums[pivot]) return pivot;
+    if (target < nums[pivot]) right = pivot - 1;
+    if (target > nums[pivot]) left = pivot + 1;
+  }
+  return target > nums[pivot] ? pivot + 1 : pivot;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 0));
