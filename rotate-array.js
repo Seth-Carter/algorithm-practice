82313c22d7ee1f18a8ab911f8a5f66bdbadcab94
Seth-Carter <carter.seth.m@gmail.com>
2021-11-11 09:39:50 +0000

    Added a rotate array algorithm

---
+++ rotate-array.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/rotate-array/
+
+// Non-mutative method
+
+const rotate = (nums, k) => {
+  const rotatedArray = [];
+  k %= nums.length;
+  for (let i = nums.length - k; i < nums.length + (nums.length - k); i++) {
+    rotatedArray.push(nums[i % nums.length]);
+  }
+  return rotatedArray;
+};
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
