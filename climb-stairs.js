bb69d51f516a4f97bef3a50b61a874bdd52082b9
Seth Carter <carter.seth.m@gmail.com>
2022-01-21 08:38:05 +0000

    Added two climb stairs solutions

---
+++ climb-stairs.js
@@ -0,0 +1,18 @@
+
+
+const climbStairs = (n, memo = { 1: 1, 2: 2 }) => {
+  // With recursion and memoization
+  // if (n in memo) return memo[n]
+  // if (n <= 0) return 0
+  // memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo)
+  // return memo[n]
+
+  // with fibonacci-style array addition
+  const output = [1,2]
+  for (let i = 2; i < n; i++) {
+    output.push(output[i-1] + output[i-2])
+  }
+
+  return output[n - 1]
+}
+ 
