7201eb44284cc60b3c50f22210f374e97fc803ed
Seth Carter <carter.seth.m@gmail.com>
2022-01-26 09:25:45 +0000

    Added a linked list palindrome detector

---
+++ is-linked-list-palindrome.js
@@ -0,0 +1,27 @@
+const isPalindrome = (head) => {
+  let fast = head;
+  let slow = head;
+  while (fast?.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+  }
+  
+  let pre = slow;
+  let current = slow.next;
+  let temp = null;
+  pre.next = null;
+  while (current) {
+      temp = current.next
+      current.next = pre
+      pre = current
+      current = temp
+  }
+
+  current = pre
+  while (current) {
+      if (head.val !== current.val) return false
+      current = current.next
+      head = head.next
+  }
+  return true
+};
