7dcbedc4ad246724f4fd886cde5d566b945b1df3
Seth Carter <carter.seth.m@gmail.com>
2022-02-16 08:42:41 +0000

    Added url for invert binary tree problem

--- invert-binary-tree.js
+++ invert-binary-tree.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
 const invertTree = (root) => {
   if (!root) return root;
   [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
