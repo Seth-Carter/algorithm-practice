b0dc4f84ac3d7b3aa318d927c90d06e6d5e01314
Seth Carter <carter.seth.m@gmail.com>
2022-02-19 11:51:46 +0000

    Added a DP approach for the sum range problem

--- sum-range.js
+++ sum-range.js
@@ -1,16 +1,26 @@
+// https://leetcode.com/problems/range-sum-query-immutable/
+
 class NumArray {
   constructor(nums) {
-      this.nums = nums;
+    this.nums = nums;
+    this.sums = this.getSums();
+  }
+  getSums() {
+    let sum = 0;
+    const sums = [0];
+
+    for (let i = 0; i < this.nums.length; i++) {
+      sum += this.nums[i];
+      sums.push(sum);
+    }
+
+    return sums;
   }
   sumRange(left, right) {
-    let sum = 0;
-    for (let i = left; i <= right; i++) {
-      sum += this.nums[i];
-    }
-    return sum;
+    return this.sums[right + 1] - this.sums[left];
   }
-};
+}
 const obj = new NumArray([-2, 0, 3, -5, 2, -1]);
-console.log(obj.sumRange(0,2))
-console.log(obj.sumRange(2,5))
-console.log(obj.sumRange(0,5))
+console.log(obj.sumRange(0, 2));
+console.log(obj.sumRange(2, 5));
+console.log(obj.sumRange(0, 5));
